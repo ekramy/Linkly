de23703d30be290f57d232d51297f30c0471c0d7
ekramy <ekramy@live.com>
2026-03-26 21:07:48 +0200

    Fianl

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
 
+btn.addEventListener("click", navToggle);
 linkForm.addEventListener('submit', formSubmit);
-
+// Toggle Moile menu
+function navToggle (){
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+}
 function isValidHttpUrl(str) {
   const pattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
